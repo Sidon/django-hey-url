c022728f6072160fa5a190632a6706c22cec6bd0
Sidon <sidoncd@gmail.com>
2022-06-22 19:44:48 -0300

    starting tests development

--- tests/utils/test_cross_helper.py
+++ tests/utils/test_cross_helper.py
@@ -1,14 +1,32 @@
-from django.test import TestCase
+from collections import namedtuple
 from django.urls import reverse
 from heyurl.utils import cross_helper
 
 
-def test_create_random_key(self):
+mock_user_agent_class = namedtuple("FakeUserAgent", ["browser", "is_mobile", "is_tablet", "is_pc"])
+
+def test_create_random_key():
     rnd_key = cross_helper._create_random_key()
     ok_key = True
     for char in rnd_key:
         if char not in cross_helper.chars:
             ok_key = False
             break
-    self.assertTrue(len(rnd_key) <= cross_helper.max_length_key and ok_key  )
+    assert len(rnd_key) <= cross_helper.max_length_key and ok_key
 
+
+def test_get_user_agent_browser_is_pc():
+    mock_user_agent = mock_user_agent_class('Mozzila Firefox', None, None, True)
+    browser, platform  =  cross_helper.get_user_agent(mock_user_agent)
+    assert browser=='Mozzila Firefox' and platform=='PC'
+
+def test_get_user_agent_is_mobile():
+    mock_user_agent = mock_user_agent_class(None, True, None, None)
+    browser, platform  =  cross_helper.get_user_agent(mock_user_agent)
+    assert not browser and platform=='Mobile'
+
+
+def test_get_user_agent_is_tablet():
+    mock_user_agent = mock_user_agent_class(None, None, True, None)
+    browser, platform  =  cross_helper.get_user_agent(mock_user_agent)
+    assert not browser and platform=='Tablet'
